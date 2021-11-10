98e4f18820d91f7a4169d44a2d2b0d1e30c7e8a6
rajudev24 <rajudevit94@gmail.com>
2021-11-10 23:44:41 +0600

    GET API

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000; 
+
+//Middlewere 
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhwuq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try{
+        await client.connect();
+        const database = client.db('Royal-Wrists');
+        const productsCollection = database.collection('products');
+
+        //GET API
+        app.get('/products', async(req, res)=>{
+            const cursor = productsCollection.find({})
+            const product = await cursor.toArray();
+            res.send(product);
+        })
+    }
+    finally{
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res)=>{
+    res.send('Running Royal Wrists Server');
+})
+
+app.listen(port, ()=>{
+    console.log('Running Royal Wrists Server on port', port)
+})
